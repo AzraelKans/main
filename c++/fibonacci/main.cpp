19306bba5f9601c9f907e1dc5d8447492451da59
AzraelKans <germancons@gmail.com>
2016-07-09 12:20:12 -0500

    small change in main

--- c++/fibonacci/main.cpp
+++ c++/fibonacci/main.cpp
@@ -1,20 +1,21 @@
-#include <iostream>
+#
+#include<iostream>
 #include "fibonacci.h"
-
  
-using namespace std;
+
+
+
  
 main()
 {
-   int n;
+   int n, c, first = 0, second = 1, next;
  
    cout << "Enter the number of terms of Fibonacci series you want" << endl;
    cin >> n;
  
    cout << "First " << n << " terms of Fibonacci series are :- " << endl;
 
-   fibonacci(n);
- 
- 
+   cout << fibonacci()
+  
    return 0;
 }
